c3928ecb1036f827bf5e3e9f93c701b198c7473e
J.H.Cho <48168432+GluteusStrength@users.noreply.github.com>
2023-01-18 01:01:04 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 3364 ms, Memory: 209548 KB -BaekjoonHub

---
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -0,0 +1,17 @@
+import sys
+v, e = map(int, sys.stdin.readline().split())
+graph = [list(map(int, sys.stdin.readline().split())) for _ in range(e)]
+graph.sort(key = lambda x : x[2])
+root = [i for i in range(v+1)]
+def find(x):
+    if root[x] == x:
+        return x
+    return find(root[x])
+res = 0
+for s, e, w in graph:
+    x = find(s)
+    y = find(e)
+    if x != y:
+        res += w
+        root[y] = x
+print(res)
